3e947de80e73c58ddab3608153a857140611d226
Wyatt Mack <wyattmack@Wyatts-MacBook-Pro.local>
2023-07-04 03:39:43 -0700

    Conditional for loop, with infinite loop defanged

---
+++ ho-031/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 0
+	for x = 0; x < 10; x++ {
+		fmt.Printf("x is %v\n", x)
+	}
+
+	// var y int
+	// for y = 0; y < 10; y-- {
+	// 	fmt.Printf("y is %v\n", y)
+	// }
+}
